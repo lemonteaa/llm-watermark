6afed54fcde3f17eee1cfc3287fffdbe4c379906
lemontea <lemontea.Tom@gmail.com>
2024-08-15 23:32:58 +0800

    Refactor I - config class and logic cleanup

--- generate.py
+++ generate.py
@@ -1,79 +1,67 @@
 import numpy as np
 import numpy.typing as npt
 
-import random
-from datetime import datetime
-#random.seed(datetime.now().timestamp())
-#random.random()
-
-# rng = np.random.default_rng(seed=42)
-# rng.random(6)
-# rng = np.random.default_rng(seed=[x for x in m.digest()])
-
 import hashlib
-#m = hashlib.sha256()
-#m.update(b"")
-#m.digest()
+
+from typing import NamedTuple
 
 from llama_cpp import Llama, LogitsProcessor, LogitsProcessorList
 
-def expand_int(n):
-    c = n % 256
-    tmp = n // 256
-    b = tmp % 256
-    a = tmp // 256
-    return [a, b, c]
-
-def expand_list(lst):
-    res = list()
-    for item in lst:
-        res += expand_int(item)
-    return res
-
 def hash_custom(lst, key):
+    def radix_expand(n, base):
+        digits = []
+        while n > 0:
+            digits.append(n % base)
+            n = n // base
+        return digits
+    def expand_list(lst, base):
+        res = list()
+        for item in lst:
+            res += radix_expand(item, base)
+        return res
     m = hashlib.sha256()
-    history = bytes(expand_list(lst))
+    history = bytes(expand_list(lst, 256))
     m.update(history)
     m.update(key.encode("utf-8"))
     return m.digest()
 
+class WaterMarkConfig(NamedTuple):
+    temperature: float
+    key: str
+    n_vocab: int
+    window_m: int
+    holdout: int
+    ep: float = 0.000001
+
 class WaterMarkingLogitsProcessor(LogitsProcessor):
-    def __init__(self, temperature, key, n_vocab, ep, window_m, holdout):
-        self.temp = temperature
-        self.key = key
-        self.n_vocab = n_vocab
-        self.ep = ep
-        self.window_m = window_m
-        self.holdout = holdout
+    def __init__(self, config: WaterMarkConfig):
+        self.config = config
         self.last_m = []
-        self.rng = np.random.default_rng()
+        self.native_rng = np.random.default_rng()
         self.first_time = True
-        #self.original_random_seed = datetime.now().timestamp()
-        self._phasetwo = False
+        self.enable = True
     
     def __call__(self, input_ids: npt.NDArray[np.intc], scores: npt.NDArray[np.single]) -> npt.NDArray[np.single]:
         if self.first_time:
             self.first_time = False
             self.prompt_tokens = len(input_ids)
-        if len(scores) != self.n_vocab:
+        if len(scores) != self.config.n_vocab:
             raise ValueError(f"score array len is {len(scores)}")
         # Find the recent history window
         outputs = input_ids[self.prompt_tokens:] # Cut the eval prompt
-        self.last_m = outputs[-self.window_m:]
-        #if len(self.last_m) >= self.window_m:
-        if len(outputs) >= self.holdout:
-            if not self._phasetwo:
-                print("[[DEBUG: ok]]")
-                self._phasetwo = True
+        self.last_m = outputs[-self.config.window_m:]
+        
+        active_rng = self.native_rng
+        if len(outputs) >= self.config.holdout and self.enable:
             # Use history window + secret key => hash => psuedo-random sampling
             # For Initial segment, sample in a normally random way (so skip)
-            digest = hash_custom(self.last_m, self.key)
-            self.rng = np.random.default_rng(seed=[x for x in digest])
+            digest = hash_custom(self.last_m, self.config.key)
+            active_rng = np.random.default_rng(seed=[x for x in digest])
         # Gumbell sampling trick
-        u = self.rng.random(self.n_vocab).astype(dtype=np.float32)
-        r = np.clip(u, self.ep, 1-self.ep)
+        u = active_rng.random(self.config.n_vocab).astype(dtype=np.float32)
+        r = np.clip(u, self.config.ep, 1-self.config.ep)
         g = -1.0 * np.log(-1.0 * np.log(r))
-        return (scores/(self.temp) + g)
+        return (scores/(self.config.temperature) + g)
 
 
 llm = Llama(
@@ -96,7 +84,9 @@ Write an essay on the role of information technology in international supply cha
 
 tokens = llm.tokenize(prompt)
 
-my_logit_proc = WaterMarkingLogitsProcessor(temperature=0.7, key="helloworld", n_vocab=llm.n_vocab(), ep=0.000001, window_m=6, holdout=30)
+my_conf = WaterMarkConfig(temperature=0.7, key="helloworld", n_vocab=llm.n_vocab(), window_m=6, holdout=30)
+
+my_logit_proc = WaterMarkingLogitsProcessor(config=my_conf)
 
 for token in llm.generate(tokens, temp=0.0, repeat_penalty=1.0, logits_processor=LogitsProcessorList([my_logit_proc])):
     #for token in llm.generate(tokens, temp=0.7, repeat_penalty=1.0):
